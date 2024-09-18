55786a22b4fee1adceef6715dbe7f607086dfd64
María Luna García <mlgarcia@uc-3-316.AluVedruna.local>
2024-09-18 19:10:49 +0200

    segundo commit?

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void lecturaFichero()throws IOException{ // excepcion usando java.IO
+            try{
+            Path file = Path.of("demo\\carpetaEjemplo\\file.txt"); // ruta al fichero
+            String content = Files.readString(file); // el contenido del fichero a String con el método readString
+            System.out.println(content); // imprime el contenido 
+            }catch(IOException e){
+                System.out.println("El archivo no ha sido encontrado");
+            }
 }
